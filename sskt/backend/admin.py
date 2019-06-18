9e6266507a9f07bae5784686141ed1654dcd617e
zqq <840486874@qq.com>
2019-06-18 23:00:24 +0800

    feat: 增加基本的权限测试
    references:
    https://docs.djangoproject.com/en/2.2/topics/auth/default/
    https://docs.djangoproject.com/en/2.2/topics/auth/customizing/
    https://blog.igevin.info/posts/django-permission/
    
    issues:
    https://stackoverflow.com/questions/39316948/typeerror-login-takes-1-positional-argument-but-2-were-given

--- sskt/backend/admin.py
+++ sskt/backend/admin.py
@@ -1,3 +1,6 @@
+from django.contrib.auth.models import Permission
 from django.contrib import admin
 
 # Register your models here.
+
+admin.site.register(Permission)
--- sskt/backend/views.py
+++ sskt/backend/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login as auth_login
 
 # Create your views here.
+
+def hello(request):
+    user = authenticate(username='person', password='ILK123456')
+
+    
+    if user is not None:
+    # A backend authenticated the credentials
+        print(user.get_all_permissions())
+        auth_login(request, user)
+    else:
+        pass
+    return HttpResponse("zqq")
+
+def login(request):
+    user = authenticate(username='person', password='ILK123456')
+    if user is not None:
+    # A backend authenticated the credentials
+        login(request.user)
+    else:
+        pass
+    # No backend authenticated the credentials
